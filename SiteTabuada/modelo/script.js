6018bd2954007a83aae41c08587e3696b24e0a49
marcialino <marcialino100@gmail.com>
2023-03-12 19:29:12 -0300

    criei site tabuada

---
+++ SiteTabuada/modelo/script.js
@@ -0,0 +1,23 @@
+function calcular(){
+    var n = window.document.getElementById('num')
+    var tab = window.document.getElementById('tab')
+   
+
+    if (n.value.length == 0){
+        window.alert('Por favor, digite um número!')
+    }else{
+        n = Number(n.value)
+        var c = 1
+        tab.innerHTML = ''
+
+        while(c <= 10){
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+
+
+        }
+    }
+}
+   
