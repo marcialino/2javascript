406d7e71f1afe02fae38e7c492fe39852c667819
marcialino <marcialino100@gmail.com>
2023-03-13 16:51:46 -0300

    Atualizei site-tabuada

--- SiteTabuada/modelo/script.js
+++ SiteTabuada/modelo/script.js
@@ -7,15 +7,20 @@ function calcular(){
         window.alert('Por favor, digite um número!')
     }else{
         n = Number(n.value)
-        var c = 1
         tab.innerHTML = ''
 
-        while(c <= 10){
+        for(var c = 1; c <=10; c++){
             var item = document.createElement('option')
+            item.text += `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            
+               
+
+            /*var item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             item.value = `tab${c}`
             tab.appendChild(item)
-            c++
+            c++*/
 
 
         }
