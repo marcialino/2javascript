1f3b8a0338eaaa7f268d8e300babf2b23e7e9b82
marcialino <marcialino100@gmail.com>
2023-03-19 10:30:26 -0300

    Criei exe001

---
+++ Aula 15variaveiscompsotas/exe001.js
@@ -0,0 +1,7 @@
+var num = [1,3,7]
+num.push(6)
+num.sort()
+
+console.log(`O nosso é ${num} `)
+console.log(`O vetor tem ${num.length} elementos`)
+console.log(`O número da terceira posição é o ${num[3]}`)
