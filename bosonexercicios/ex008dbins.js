5945e7fa961ed14570d98675e4288c976f3d9cc6
marcialino <marcialino100@gmail.com>
2023-03-23 16:50:59 -0300

    criei ex008dbins

---
+++ bosonexercicios/ex008dbins.js
@@ -0,0 +1,18 @@
+var somaimpar = 0
+for(var c = 1; c <=30; c+=2){
+somaimpar +=c
+}
+console.log(somaimpar)
+
+var somapar = 0
+for (var c=0; c<=30; c+=2){
+    somapar +=c
+}
+console.log(somapar)
+
+var mult = 1
+for(var c=1; c<=6; c++){
+    mult *=c
+}
+console.log(mult)
+
