d0315da23a9bb2132dc8921047f295d375a54bf8
marcialino <marcialino100@gmail.com>
2023-03-23 17:09:43 -0300

    criei ex009dbins

---
+++ bosonexercicios/ex009dbins.js
@@ -0,0 +1,10 @@
+function converte(temp){
+ return (temp*9/5) + 32
+}
+var tempnova = converte(20)
+console.log(tempnova)
+
+function convert(tempF){
+return (tempF-32)*5/9
+}
+console.log(convert(68))
