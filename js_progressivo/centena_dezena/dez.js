6a4f220a642add12d45c090bb9e99e1fc8d8f41e
marcialino <marcialino100@gmail.com>
2023-04-08 07:32:40 -0300

    atualizei

--- js_progressivo/centena_dezena/dez.js
+++ js_progressivo/centena_dezena/dez.js
@@ -3,30 +3,14 @@ function clicar(){
 
     var res = window.document.getElementById('res')
     
-    var div = num/2
-
-    var cem = ''
-    var dez = ''
-    var un = ''
-
-    if (num.length == 3){
-          cem = (num.length)
-          dez = (num.length)-1
-          un = (num.length) -2
-          
-          res.innerHTML = `O número ${num} tem:<br> ${cem}centenas <br> ${dez} dezenas <br> ${un} unidades`
-
+    /*Transformando o número em uma array*/ 
+    n = Array.from(String(num)) 
+   
+    if (n.length == 3){
+        res.innerHTML = `O número <strong>${num}</strong> tem <strong>${n[0]}</strong> centenas, <strong>${n[1]}</strong> dezenas e <strong>${n[2]}</strong> unidades. `
+    }if (n.length == 2){
+        res.innerHTML = `O número <strong>${num}</strong> tem <strong>${n[0]}</strong> dezenas e <strong>${n[1]}</strong> unidades.`
+    }if(n.length == 1){
+        res.innerHTML = `O número <strong>${num}</strong> tem <strong>${n[0]}</strong> unidades.`
     }
-    if (num.length = 2){
-         dez = num.length
-         un = num.length - 1
-         
-         res.innerHTML = `O número ${num} tem:<br> ${dez} dezenas <br> ${un} unidades`
-
-    }if (num.length = 1){
-        un = num.length
-        res.innerHTML = `O número ${num} tem:<br> <br> ${un} unidades`
-
-    }
-    
 }
