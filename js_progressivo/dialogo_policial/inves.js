edb33a96577f15fdf61fc51358db6be6ae0edbdb
marcialino <marcialino100@gmail.com>
2023-04-09 11:26:43 -0300

    criei dialogo_policial

---
+++ js_progressivo/dialogo_policial/inves.js
@@ -0,0 +1,12 @@
+function enviar(){
+    var sim1 = window.document.getElementById('sim1')
+    var sim2 = window.document.getElementById('sim2')
+    var sim3 = window.document.getElementById('sim3')
+    var sim4 = window.document.getElementById('sim4')
+    var sim5 = window.document.getElementById('sim5')
+    var nao1 = window.document.getElementById('nao1')
+    var nao2 = window.document.getElementById('nao2')
+    var nao3 = window.document.getElementById('nao3')
+    var nao4 = window.document.getElementById('nao4')
+    var nao5 = window.document.getElementById('nao5')
+}
