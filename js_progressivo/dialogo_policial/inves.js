e289283423b54aab26dc2be23c0b21a33b9b939e
marcialino <marcialino100@gmail.com>
2023-04-09 15:10:32 -0300

    atualizei

--- js_progressivo/dialogo_policial/inves.js
+++ js_progressivo/dialogo_policial/inves.js
@@ -1,5 +1,7 @@
 function enviar(){
+    var confirm = ''
     var sim1 = window.document.getElementById('sim1')
+   
     var sim2 = window.document.getElementById('sim2')
     var sim3 = window.document.getElementById('sim3')
     var sim4 = window.document.getElementById('sim4')
@@ -9,4 +11,13 @@ function enviar(){
     var nao3 = window.document.getElementById('nao3')
     var nao4 = window.document.getElementById('nao4')
     var nao5 = window.document.getElementById('nao5')
+
+    var res = window.document.getElementById('res')
+
+    sim1.addEventListener("click", function(){
+        confirm = "sim"
+    })
+    
+res.innerHTML = `${confirm}`
+   
 }
