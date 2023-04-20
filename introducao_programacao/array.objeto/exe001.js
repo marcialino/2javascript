558aa5880c992b19b512135e22a8763d2fd3fc56
marcialino <marcialino100@gmail.com>
2023-04-20 16:43:08 -0300

    aturalizei

---
+++ introducao_programacao/array.objeto/exe001.js
@@ -0,0 +1,34 @@
+var objeto ={
+    residuo: 'Triptofano',
+    codigo:'W',
+    tipo: 'Polar',
+    atomos: 27
+}
+
+console.log(objeto)
+
+var carro = {
+    marca: 'KA',
+    cor: 'Branco',
+    ano: 2022,
+    conservação: 'novo'
+}
+console.log(carro)
+
+var natacao = {
+    esporte: 'Natação',
+    dificuldade: 'média',
+    recomendação: 'todas as idades',
+    benefícios: 'bem-estar aeróbico e massa corporal'
+
+}
+console.log(natacao)
+
+var idade = {
+    criança: 'até 10 anos',
+    adolescente: 'de 11 até 17 anos',
+    adulto: 'acima de 18 a 59 anos',
+    idoso: 'acima de 60 anos'
+
+}
+console.log(idade)
---
+++ introducao_programacao/array.objeto/exe002.js
@@ -0,0 +1,11 @@
+/* array em JS pode ser considerado um tipo especial de objeto. As array são declaradas entre colchetes, os objetos entre chaves e devem apresentar pares de nomes e valores. */
+
+var array_1 = [1,5,4,3]
+var array_2 = [6,7,8,9]
+
+console.log(array_1)
+console.log(array_2)
+console.log(array_1 + "," + array_2)
+console.log(array_1.length)
+console.log(array_2.length)
+console.log(array_1.sort())
