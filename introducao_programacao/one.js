46e20d50be918dac35be802bedbbc55b77bbdef7
marcialino <marcialino100@gmail.com>
2023-04-21 18:28:26 -0300

    crieiexe010_eventos

---
+++ introducao_programacao/one.js
@@ -0,0 +1,30 @@
+//onload
+
+function carregado (){
+    alert('A página já está carregada!')
+}
+
+//onclick
+
+function fuiClicado(){
+    alert('Clicaram em mim!')
+}
+
+//onmouseover
+
+function mouseEmCima(){
+    alert('Tem um mouse em mim!')
+
+}
+//onmouseout
+function mouseFora(){
+    alert('Ufa!, ele já saiu')
+}
+
+// onkeydown
+function tecladoUsado(){
+    alert('Ei, não digita em mim! Não gosto de letras Tente digitar a caixa de texto vizinha!')
+}
+function mudanca(){
+    alert('Não me abandone')
+}
