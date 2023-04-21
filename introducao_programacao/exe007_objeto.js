0e3c02c23f0a4ce5e07310f27528a332d7fd3c43
marcialino <marcialino100@gmail.com>
2023-04-21 17:47:59 -0300

    atualizei

---
+++ introducao_programacao/exe007_objeto.js
@@ -0,0 +1,8 @@
+var notebook = {
+marca: 'Lenovo',
+cor: 'Cinza',
+Fabricação: 2020,
+Qualidade: 'Muito boa'
+
+}
+console.log(notebook)
---
+++ introducao_programacao/exe008_array.js
@@ -0,0 +1,14 @@
+var a = [1,2,3,4,5]
+var b = [6,7,8,9,10]
+var c = ['Ana', 'José', 'Carlos', 'Maria', 'Luana', 'Tereza']
+var c1 = c.sort()
+
+
+console.log(a)
+console.log(a.length)
+console.log(b)
+console.log(b.length)
+console.log(a + "," + b)
+console.log(a[1])
+console.log(b[0])
+console.log(c1)
